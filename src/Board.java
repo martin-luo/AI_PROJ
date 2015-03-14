3da6b333971a938f787c789c143c2d8816ac3beb
Bingfeng Liu <liubingfeng93@gmail.com>
2015-03-14 14:09:26 +1100

    rows checking added

--- src/Board.java
+++ src/Board.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 
 public class Board 
 {
+	public static String WHITE="W";
+	public static String BLACK="B";
+	public static String CAPTURED="-";
+	public static String FREE="+";
 	String inputFileName;
 	int boardDimension=0;
 	int freeCell=0;
@@ -20,36 +24,64 @@ public class Board
 		
 	}
 	
+	public String getBoardCell(int row,int col)
+	{
+		return boardBody[row][col];
+	}
+	
+	public void setBoardCell(int row,int col,String sign)
+	{
+		boardBody[row][col]=sign;
+	}
+	
+	public void checkRowNumber(int rowCount)
+	{
+		if (rowCount==boardDimension)
+		{
+			return;
+		}
+		
+		else
+		{
+			System.out.println ("X.X ---> Error:Actual Rows and Board Dimension Mismatched.");
+			System.exit(0);
+		}
+		
+	}
+	
 	public void doParseInput()
 	{
-		int col=0,row=0;
+		int col=0,row=0,rowCount=0;
 		initializeBoardBody(boardDimension);
 		ArrayList<String> tempStringArray=new ArrayList<String>();
 		String[] parts=null;
 		try
 		{
-
 	         // FileReader inputFile = new FileReader("file name");
-
 	          BufferedReader bufferReader = new BufferedReader(new InputStreamReader(System.in));
-
 	          String line;
-
+	          //add sign into a string array by split each line with delimit ' '
 	          while ((line = bufferReader.readLine()) != null)
 	          {
 	            parts=line.split(" ");
 	            for(int i=0 ;i<parts.length;i++)
 	            {
 	            	tempStringArray.add(parts[i]);
+	            	rowCount++;
 	            }
 	          }
 
 	          bufferReader.close();
-	      }
+	     }
 		catch(Exception e)
 		{
 	          System.out.println("Error while reading file line by line:" + e.getMessage());                      
 	    }
+		
+		checkRowNumber(rowCount);
+		
+		
+		
 		boardDimension=Integer.parseInt(tempStringArray.remove(0));
 		initializeBoardBody(boardDimension);
 		
@@ -63,7 +95,6 @@ public class Board
 			}
 		}
 		printBoardBody(boardBody);
-		
 	}
 	
 	public void printBoardBody(String boardBody[][])
